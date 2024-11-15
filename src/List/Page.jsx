48ee7f5a7c0182c9435680ff55e1889bf3469dca
Gustavo-Pereira <gupesouza0@gmail.com>
2024-11-15 16:39:37 -0300

    adição da estilização

--- src/List/Page.jsx
+++ src/List/Page.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { List } from "./List";
+import './style.css'
 export function Page() {
     const [tarefa, SetTarefa] = useState('');
     const [list, SetList] = useState([]);
@@ -33,12 +34,19 @@ export function Page() {
                     onChange={PegandoInform}
                     value={tarefa}
                 />
-                <button type="submit">Adicionar</button>
+                <button type="submit">
+                    <span class="circle1"></span>
+                    <span class="circle2"></span>
+                    <span class="circle3"></span>
+                    <span class="circle4"></span>
+                    <span class="circle5"></span>
+                    <span class="text">Submit</span>
+                </button>
             </form>
 
             {/* importando a lista com dados desta pagina */}
             <List list={list} />
-    
+
         </div>
     );
 }
